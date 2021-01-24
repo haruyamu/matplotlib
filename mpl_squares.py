9b6aa7c9a636f16fed8c6e0d8aa0e3be3ff60d33
haruyamu <haruya.20020331@icloud.com>
2021-01-24 15:45:55 +0900

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3, 4, 5]
+
+plt, style.use('seaborn')
+fig, ax = pltsubplots()
+ax.plot(squares, input_values, linewidth=3)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
